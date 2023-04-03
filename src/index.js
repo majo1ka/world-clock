dc14d060e88390a5dc9c9dc14c5acdeb532a8c61
Maja <85358169+majo1ka@users.noreply.github.com>
2023-04-03 16:20:34 +0200

    functional city select

--- src/index.js
+++ src/index.js
@@ -2,20 +2,38 @@ function updateTime() {
 
 //New York
 let newYorkElement = document.querySelector("#new-york");
+if (newYorkElement){
 let newYorkDateElement = newYorkElement.querySelector(".date");
 let newYorkTimeElement = newYorkElement.querySelector(".time");
 let newYorkTime = moment().tz("America/New_York");
 
 newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
 newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss [<small>]A[</small>]");
-
+};
 //Tokyo
 let tokyoElement = document.querySelector("#tokyo");
+if (tokyoElement){
 let tokyoDateElement = tokyoElement.querySelector(".date");
 let tokyoTimeElement = tokyoElement.querySelector(".time");
 let tokyoTime = moment().tz("Asia/Tokyo");
 
 tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
 tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
-};
+}};
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML =
+    `<div class="city">
+        <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+        </div>`
+}
 setInterval(updateTime, 1000);
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
